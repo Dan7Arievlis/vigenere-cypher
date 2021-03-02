2ac0119de1270755881762712f4922dc60e25c49
Daniel Neves <73949890+Dan7Arievlis@users.noreply.github.com>
2021-03-02 16:20:47 -0300

    Esses são os arquivos do código
    Tá tudo aí

---
+++ Principal.java
@@ -0,0 +1,14 @@
+/* ***************************************************************
+* Autor: Daniel Neves Brasileiro Costa Silveira
+* Matricula: 201911913
+* Inicio: 26/02/2021
+* Ultima alteracao: 02/03/2021
+* Nome: Principal
+* Funcao: Inicializa o programa e a janela principal
+*************************************************************** */
+
+public class Principal {
+  public static void main(String[] args) {
+    new MainFrame();  //inializa a janela
+  } // fim do metodo main
+} // fim da classe Principal
