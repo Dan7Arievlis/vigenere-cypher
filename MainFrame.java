f6b1b222af0d594f5a3eed3678afa4d1bc3098ed
Daniel Neves <73949890+Dan7Arievlis@users.noreply.github.com>
2021-09-03 15:39:43 -0300

    Update MainFrame.java

--- MainFrame.java
+++ MainFrame.java
@@ -1,6 +1,5 @@
 /* ***************************************************************
-* Autor: Daniel Neves Brasileiro Costa Silveira
-* Matricula: 201911913
+* Autor: Dan7Arievlis
 * Inicio: 26/02/2021
 * Ultima alteracao: 02/03/2021
 * Nome: MainFrame
@@ -47,7 +46,7 @@ public class MainFrame extends JFrame {
     panel.getDecryptedTextField().addCaretListener(a -> {  // executa a toda alteracao feita na caixa de texto da mensagem decifrada
       panel.getLimitDecrypt().setText(panel.refreshDecryptLimit());  // atualiza a label dessa caixa de texto
     });
-  } // fim do construtor
+  }
 
 /* ***************************************************************
 * Metodo: init
@@ -68,7 +67,7 @@ public class MainFrame extends JFrame {
     setLocationRelativeTo(null);  // a janela comeca no centro da tela
     setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);  // encerra a execucao ao fechar a janela
     this.setVisible(true);  // torna a janela visivel
-  } // fim do metodo init
+  }
    
 /* ***************************************************************
 * Metodo: encryptButtonAction
@@ -77,12 +76,12 @@ public class MainFrame extends JFrame {
 * Retorno: void
 *************************************************************** */
   public void encryptButtonAction() {
-    if(!panel.getEncryptedTextField().getText().isEmpty()) {  // condicional para apagar a outra caixa de texto
+    if(!panel.getEncryptedTextField().getText().isEmpty())  // condicional para apagar a outra caixa de texto
       panel.getEncryptedTextField().setText("");
-    }  // fim do if
+    
     panel.getCypher().encrypt(panel.getDecryptedTextField().getText());  // criptografa mensagem na caixa de texto
     panel.getEncryptedTextField().setText(panel.getCypher().getEncrypted().toString());  // escreve a mensagem criptografada na outra caixa
-  } // fim do metodo encryptButtonAction
+  }
 
 /* ***************************************************************
 * Metodo: decryptButtonAction
@@ -91,10 +90,10 @@ public class MainFrame extends JFrame {
 * Retorno: void
 *************************************************************** */
   public void decryptButtonAction() {
-    if(!panel.getDecryptedTextField().getText().isEmpty()) {  // condicional para apagar a outra caixa de texto
+    if(!panel.getDecryptedTextField().getText().isEmpty())  // condicional para apagar a outra caixa de texto
       panel.getDecryptedTextField().setText("");
-    }  // fim do if
+    
     panel.getCypher().decipher(panel.getEncryptedTextField().getText());  // decifra mensagem na caixa de texto
     panel.getDecryptedTextField().setText(panel.getCypher().getDecrypted().toString());  // escreve a mensagem decifrada na outra caixa
-  } // fim do metodo decryptButtonAction
-} // fim da classe MainFrame
+  }
+}
