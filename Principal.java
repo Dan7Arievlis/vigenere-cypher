b0a0de69a95b69b1b3860b36eaa3087f1bc8b7f0
Daniel Neves <73949890+Dan7Arievlis@users.noreply.github.com>
2021-09-03 15:44:02 -0300

    Update Principal.java

--- Principal.java
+++ Principal.java
@@ -1,6 +1,5 @@
 /* ***************************************************************
-* Autor: Daniel Neves Brasileiro Costa Silveira
-* Matricula: 201911913
+* Autor: Dan7Arievlis
 * Inicio: 26/02/2021
 * Ultima alteracao: 02/03/2021
 * Nome: Principal
@@ -9,6 +8,6 @@
 
 public class Principal {
   public static void main(String[] args) {
-    new MainFrame();  //inializa a janela
-  } // fim do metodo main
-} // fim da classe Principal
+    new MainFrame();
+  }
+}
